7c1841b09bd37d97935ab5a57eca08ce8e8ac325
Mathew Theberge <140298808+Mathew-Theberge@users.noreply.github.com>
2024-08-20 00:17:47 -0400

    add event listener to store form data as an object in myLibrary array

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const modal = document.querySelector("[data-modal]")
 const newBook = document.querySelector(".new-book")
-
+const form = document.querySelector("form")
 const myLibrary = []
 
 function Book(title, author, pages, isRead) {
@@ -17,3 +17,13 @@ function addBooktoLibrary() {
 newBook.addEventListener("click", () => {
 	modal.showModal()
 })
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault()
+	const formData = new FormData(form)
+	const formDataObj = Object.fromEntries(formData)
+	myLibrary.push(formDataObj)
+	form.reset()
+	modal.close()
+})
+
